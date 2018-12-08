8039afef5e9eabb23edf203b0e47a62040c77e97
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2018-12-08 19:55:11 +0530

    werrew

--- contract/fmcgct/scripts/eosCreate.sh
+++ contract/fmcgct/scripts/eosCreate.sh
@@ -15,5 +15,4 @@ cleos --print-request --url http://127.0.0.1:7777 get info
 cleos --print-response --url http://127.0.0.1:7777 get account eosio
 cleos --url http://127.0.0.1:7777 create account eosio dunya EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
 cleos --url http://127.0.0.1:7777 create account eosio hello EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-cleos unlock
-/eosnagc/itc/bc_eos_itc/contract/fmcgct/deploy.sh
+/eosnagc/itc/bc_eos_itc/contract/fmcgct/scripts/deploy.sh

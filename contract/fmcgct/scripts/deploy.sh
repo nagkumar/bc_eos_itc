70f31d83ddf4bce92304cc6ff2b665e50e607782
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2018-12-09 07:08:46 +0530

    werrew

--- contract/fmcgct/scripts/deploy.sh
+++ contract/fmcgct/scripts/deploy.sh
@@ -4,7 +4,8 @@ cd /eosnagc/itc/bc_eos_itc/contract/fmcgct
 eosio-cpp -o fmcgct.wasm fmcgct.cpp --abigen
 cd ..
 cleos --url http://127.0.0.1:7777 set contract hello ./fmcgct -p hello@active
-cleos --url http://127.0.0.1:7777 push action hello promote '["mg121", "crd","itc", "mfr"]' -p hello@active
+cd fmcgct
+cleos --url http://127.0.0.1:7777 push action hello promote '["mg12", "crd","itc", "mfr"]' -p hello@active
 cleos --url http://127.0.0.1:7777 get table hello hello fmcg
 
 
--- contract/fmcgct/scripts/eosCreate.sh
+++ contract/fmcgct/scripts/eosCreate.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
-apt-get upgrade
-apt-get update
-apt-get install iputils-ping
-apt-get install net-tools
+
+#docker run --name eosio -v e:/eosnagw:/eosnagc --publish 7777:7777 --publish 127.0.0.1:5555:5555 --detach eosio/eos:v1.4.1 /bin/bash -c "keosd --http-server-address=0.0.0.0:5555  & exec nodeos -e -p eosio --plugin eosio::producer_plugin --plugin eosio::chain_api_plugin --plugin eosio::history_plugin --plugin eosio::history_api_plugin --plugin eosio::http_plugin -d /mnt/dev/data --config-dir /mnt/dev/config --http-server-address=0.0.0.0:7777 --access-control-allow-origin=* --contracts-console --http-validate-host=false --filter-on='*'"
+#docker exec -d eosio /eosnagc/itc/bc_eos_itc/contract/fmcgct/scripts/eosCreate.sh
+
+apt-get upgrade -y
+apt-get update -y
+apt-get install iputils-ping -y
+apt-get install net-tools -y
 apt-get -y install curl
-apt-get install -y wget   && rm -rf /var/lib/apt/lists/*
+apt-get install -y wget && rm -rf /var/lib/apt/lists/*
 
 ifconfig -a
-ping localhost
+ping localhost -c 4
 cleos wallet create --to-console
 cleos wallet create_key
 cleos --url http://127.0.0.1:7777 get info
@@ -17,6 +21,12 @@ cleos --print-request --url http://127.0.0.1:7777 get info
 cleos --print-response --url http://127.0.0.1:7777 get account eosio
 cleos --url http://127.0.0.1:7777 create account eosio dunya EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
 cleos --url http://127.0.0.1:7777 create account eosio hello EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-wget https://github.com/eosio/eosio.cdt/releases/download/v1.4.1/eosio.cdt-1.4.1.x86_64.deb
-apt install ./eosio.cdt-1.4.1.x86_64.deb
-/eosnagc/itc/bc_eos_itc/contract/fmcgct/scripts/deploy.sh
+cleos --url http://127.0.0.1:7777 create account eosio fmcgct EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
+
+#wget https://github.com/eosio/eosio.cdt/releases/download/v1.4.1/eosio.cdt-1.4.1.x86_64.deb
+apt install /eosnagc/dld/eosio.cdt-1.4.1.x86_64.deb
+cd /eosnagc/itc/bc_eos_itc/contract/fmcgct/scripts/
+./deploy.sh
+./deploy.sh
+cd ..
+pwd
